3b38e4d74d81366fe96ff5d5e34f46b6797b1884
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2021-11-08 23:33:38 -0600

    Added homework of lesson 2 and lesson3

---
+++ Lesson 003/Score.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT 
+// Author: Constantino Mora
+// Date: Nov 8, 2021
+
+pragma solidity ^0.8.0;
+
+contract Score {
+    
+    uint score;
+    address owner;
+    
+    mapping(address => uint) public score_list;
+    
+    event NewScore(uint _newScore);
+    
+    constructor() {
+        owner = msg.sender;    
+    }
+    
+    modifier onlyOwner {
+        if(msg.sender == owner) {
+            _;
+        }
+            
+    }
+    
+    function getScore() public view returns(uint) {
+        return score;
+    }
+    
+    function setScore(uint _score) public onlyOwner {
+        score = _score;
+        
+        score_list[msg.sender] = score;
+        
+        emit NewScore(score);
+    }
+}
