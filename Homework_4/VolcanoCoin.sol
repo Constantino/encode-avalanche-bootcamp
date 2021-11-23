1ff9a559cfb68a305083555b0bfc6f48f21b3667
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2021-11-23 17:58:03 -0600

    Added homework 4

---
+++ Homework_4/VolcanoCoin.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+// Author: Constantino Mora
+// Homework 3
+
+pragma solidity ^0.8.0;
+
+contract VolcanoCoin {
+    
+    uint total_supply = 10000;
+    address owner;
+    mapping(address => uint256) public balance;
+
+    constructor() {
+        owner = msg.sender;
+        balance[owner] = total_supply;
+    }
+    
+    modifier onlyOwner {
+        if(msg.sender == owner) {
+            _;
+        }
+            
+    }
+
+    struct Payment {
+        uint amount;
+        address payable recipent;
+    }
+
+    mapping(address => Payment[]) public payments;
+    
+    event TotalSupplyIncreased(uint);
+    event Transfer(uint, address);
+    
+    function getTotalSupply() public view returns(uint) {
+        return total_supply;
+    }
+    
+    function increaseTotalSupply() public onlyOwner{
+        total_supply += 1000;
+        emit TotalSupplyIncreased(total_supply);
+    }
+
+    function transfer(uint _amount, address payable _recipient) public {
+        balance[msg.sender] -= _amount;
+        balance[_recipient] += _amount;
+
+        payments[msg.sender].push(Payment(_amount, _recipient));
+
+        emit Transfer(_amount, _recipient);
+    }
+}
